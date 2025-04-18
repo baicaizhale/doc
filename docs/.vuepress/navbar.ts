7fb397c6688d827739b52a855d02a10237c5a930
Yan Cai <2651557041@qq.com>
2025-04-19 06:46:43 +0800

    Update site icon and README for improved branding and clarity

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -6,7 +6,7 @@ export const navbar = defineNavbarConfig([
   { text: '标签', link: '/blog/tags/' },
   { text: '归档', link: '/blog/archives/' },
   {
-    text: '笔记',
+    text: '文档',
     items: [{ text: '示例', link: '/notes/demo/README.md' }]
   },
 ])
