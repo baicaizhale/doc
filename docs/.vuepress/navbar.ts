89ace888a1fbe071b3e64b758c0a6559c8645494
Yan Cai <110255748+baicaizhale@users.noreply.github.com>
2025-04-20 15:25:46 +0800

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,12 +1,12 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: '/' },
-  {
-    text: '文档',
-    items: [
-      { text: '示例', link: '/notes/demo/README.md' }
-    ]
-  },
+  { text: '首页', link: '/note' },
+//  {
+//    text: '文档',
+//    items: [
+//      { text: '示例', link: '/notes/demo/README.md' }
+//    ]
+//  },
   { text: '帮助', link: '/help/' } // 改为自动生成help笔记
 ])
